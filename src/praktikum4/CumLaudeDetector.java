f52be774917c911b9141766e07f658c82ca8f511
Rando Rommot <rrommot@itcollege.ee>
2016-09-08 20:40:26 +0300

    kodul2ppar setup vol 2

--- src/praktikum4/CumLaudeDetector.java
+++ src/praktikum4/CumLaudeDetector.java
@@ -6,13 +6,13 @@ package praktikum4;
 public class CumLaudeDetector {
     public static void main (String[] args) {
         System.out.println("Sisesta oma kaalutud keskhinne: ");
-        Double averageMark = TextIO.getlnDouble();
+        double averageMark = TextIO.getlnDouble();
         System.out.println("Sisesta oma lõputöö hinne: ");
-        Double finalPaperMark = TextIO.getlnDouble();
+        int finalPaperMark = TextIO.getlnInt();
 
         if ((averageMark > 4.5 && averageMark <= 5) && finalPaperMark == 5) {
             System.out.println("Jah saad cum laude diplomile!");
-        }else if ((averageMark <= 4.5 && averageMark >= 0) && (finalPaperMark > 0 && finalPaperMark < 6)){
+        }else if ((averageMark <= 4.5 && averageMark >= 0) && (finalPaperMark > 0 && finalPaperMark <= 5)){
             System.out.println("Ei saa cum laudet :(");
         }else{
             System.out.println("Hinne peab olema 0 ja 5 vahel");
