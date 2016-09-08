c12fd603ebfe3c20b39014e2d922e65f164e7e45
Rando Rommot <rrommot@klass2-14-l.intra.itcollege.ee>
2016-09-08 12:19:57 +0300

    eelmised praksid

---
+++ src/praktikum1/Tere.java
@@ -0,0 +1,10 @@
+package praktikum1;
+public class Tere {
+    public static void main (String[] args) {
+        System.out.println ("*****  *****  ****   *****  *        *");
+        System.out.println ("  *    *      *   *  *      *   **    *");
+        System.out.println ("  *    *****  * **   *****  *          *");
+        System.out.println ("  *    *      * *    *          **    *");
+		System.out.println ("  *    *****  *  *   *****  *       *");
+    }
+}
---
+++ src/praktikumid2/NameLength.java
@@ -0,0 +1,10 @@
+package praktikumid2;
+
+public class NameLength {
+	public static void main(String[] args) {
+		System.out.println("Mis su nimi on?");
+		String name = TextIO.getlnString();
+		int nameLength = name.length();
+		System.out.println("Su koleda nime pikkus on " + nameLength + " t2hem2rki");
+	}
+}
