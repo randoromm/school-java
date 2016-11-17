8617eef9754f5c0691e049bb5266e4147ed4f94d
Rando Rommot <rrommot@itcollege.ee>
2016-11-17 02:56:36 +0200

    Praktikum 13 done

--- src/praktikum13/Matrices.java
+++ src/praktikum13/Matrices.java
@@ -14,15 +14,93 @@ public class Matrices {
                 {5, 6, 7, 8, 9},
         };
 
+        int[][] morpheus = {
+                {1, 2},
+                {3, 4},
+                {5, 6},
+        };
+
         printOneDimArray(neo[1]);
-        System.out.println();
+
         printMatrice(neo);
-        System.out.println();
-        sumOfRows(neo);
+
+        printOneDimArray(sumOfRows(neo));
+
+        System.out.println(sumOfSecondaryDiagonal(neo));
+
+        printOneDimArray(maxOfRows(neo));
+
+        System.out.println(minimumOfMatrice(neo));
+
+        printMatrice(modulusMatrice(3, 5));
+
+        printMatrice(transpose(morpheus));
     }
 
-    private static void sumOfRows(int[][] neo) {
-        // TODO
+    private static int[][] transpose(int[][] matrice) {
+        int[][] result = new int[matrice[0].length][matrice.length];
+        for (int y = 0; y < matrice.length; y++) {
+            for (int x = 0; x < matrice[y].length; x++) {
+                result[x][y] = matrice[y][x];
+            }
+        }
+        return result;
+    }
+
+    private static int[][] modulusMatrice(int rows, int columns) {
+        int[][] result = new int[rows][columns];
+        for (int y = 0; y < rows; y++) {
+            for (int x = 0; x < columns; x++) {
+                result[y][x] = (y + x) % 2;
+            }
+        }
+        return result;
+    }
+
+    private static int minimumOfMatrice(int[][] matrice) {
+        int result = matrice[0][0];
+        for (int[] array : matrice) {
+            for (int element : array) {
+                if (element < result) result = element;
+            }
+        }
+        return result;
+    }
+
+    private static int[] maxOfRows(int[][] matrice) {
+        int[] result = new int[matrice.length];
+
+        for(int i = 0; i < matrice.length; i++) {
+            int max = matrice[i][0];
+            for (int element : matrice[i]) {
+                if (element > max) max = element;
+            }
+            result[i] = max;
+        }
+
+        return result;
+    }
+
+    private static int sumOfSecondaryDiagonal(int[][] matrice) {
+        int result = 0;
+        int i = matrice[0].length - 1;
+        for (int[] array : matrice) {
+            result += array[i];
+            i--;
+        }
+        return result;
+    }
+
+    private static int[] sumOfRows(int[][] matrice) {
+        int[] result = new int[matrice.length];
+        for (int k = 0; k < matrice.length; k++) {
+            int sum = 0;
+            for (int i : matrice[k]) {
+                sum += i;
+            }
+            result[k] = sum;
+        }
+        return result;
     }
 
     private static void printOneDimArray(int[] array) {
