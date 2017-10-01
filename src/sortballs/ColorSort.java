5d144dda1fea516a66caef41ab07a6935174989e
Rando R <rando_rommot@live.com>
2017-10-01 22:18:28 +0300

    homework done

--- src/sortballs/ColorSort.java
+++ src/sortballs/ColorSort.java
@@ -1,11 +1,17 @@
 package sortballs;
 
 
+import java.util.Random;
+
 public class ColorSort {
 
     enum Color {red, green, blue}
 
     public static void main(String[] param) {
+        Color[] examp = genRandomRow(20);
+        printRow(examp);
+        reorder(examp);
+        printRow(examp);
 
     }
 
@@ -13,22 +19,45 @@ public class ColorSort {
         int redCount = 0;
         int blueCount = balls.length - 1;
 
-        for(int i=0; i<=blueCount; i++){
-            if(redCount < blueCount){
+        for(int i = 0; i <= blueCount; i++){
+            if(redCount < blueCount){  // Everything is already sorted if that happens
                 if(balls[i] == Color.red){
-                    Color tmp = balls[i];
+                    Color temp = balls[i];
                     balls[i] = balls[redCount];
-                    balls[redCount] = tmp;
-                    redCount++;
+                    balls[redCount] = temp;
+                    redCount++;  // No i--, because the replaced ball can't be blue anyways
                 }
                 else if(balls[i] == Color.blue){
-                    Color tmp = balls[i];
+                    Color temp = balls[i];
                     balls[i] = balls[blueCount];
-                    balls[blueCount] = tmp;
+                    balls[blueCount] = temp;
                     blueCount--;
-                    i--;
+                    i--;  // Because the replaced ball may be red
                 }
             }
         }
+    }
+
+    public static Color[] genRandomRow(int len) {
+        Color[] res = new Color[len];
+        for (int i = 0; i < len; i++) {
+            int r = new Random().nextInt(2 + 1);
+            if (r == 0) {
+                res[i] = Color.red;
+            } else if (r == 1) {
+                res[i] = Color.green;
+            } else if (r == 2) {
+                res[i] = Color.blue;
+            }
+
+        }
+        return res;
+    }
+
+    public static void printRow(Color[] row) {
+        for (Color c : row) {
+            System.out.print(c + " ");
+        }
+        System.out.println();
     }
 }
