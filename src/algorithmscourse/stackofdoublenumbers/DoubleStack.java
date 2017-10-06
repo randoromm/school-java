5c35233f4660523b4df27af14735108b63c05759
Rando R <rando_rommot@live.com>
2017-10-06 08:25:37 +0300

    Hw almost finished

--- src/algorithmscourse/stackofdoublenumbers/DoubleStack.java
+++ src/algorithmscourse/stackofdoublenumbers/DoubleStack.java
@@ -13,9 +13,12 @@ public class DoubleStack {
         System.out.println(test.toString());
         System.out.println(test.pop());
         System.out.println(test.toString());
+        test.op("+");
+        System.out.println(test.toString());
+        System.out.println(interpret("5 5 5 + 2 5"));
     }
 
-    public LinkedList<Double> stack;
+    private LinkedList<Double> stack;
 
     DoubleStack() {
         stack = new LinkedList<>();
@@ -24,45 +27,42 @@ public class DoubleStack {
     @Override
     public Object clone() throws CloneNotSupportedException {
         DoubleStack clonedObj = new DoubleStack();
-        LinkedList clonedList = (LinkedList<Double>)stack.clone();
-        clonedObj.stack = clonedList;
-        return clonedObj; // TODO!!! java.lang.AssertionError: clone must be equal to original; (even tho it is)
+        clonedObj.stack = (LinkedList<Double>)stack.clone();
+        return clonedObj;
     }
 
     public boolean stEmpty() {
         return stack.isEmpty();
     }
 
-    public void push (double a) {
+    public void push(double a) {
         stack.push(a);
-    } // TODO!!! Deal with overflow?
+    }
 
     public double pop() {
         if (stEmpty()) throw new RuntimeException();
-        return stack.pop(); // TODO!!! Confirm exception.
-    } // pop
+        return stack.pop();
+    }
 
     public void op (String s) {
-        double first = stack.pop();
-        double second = stack.pop();
+        double first = pop();
+        double second = pop();
 
-        if (s.equals("+")) stack.push(first + second);
-        if (s.equals("-")) stack.push(first - second);
-        if (s.equals("*")) stack.push(first * second);
-        if (s.equals("/")) stack.push(first / second);
+        if (s.equals("+")) push(second + first);
+        if (s.equals("-")) push(second - first);
+        if (s.equals("*")) push(second * first);
+        if (s.equals("/")) push(second / first);
     }
 
     public double tos() {
+        if (stEmpty()) throw new RuntimeException();
         return stack.peek();
     }
 
     @Override
     public boolean equals (Object o) {
-        if (o == this) return true;
-        if (o.getClass() != this.getClass()) return false;
-        DoubleStack tmp = (DoubleStack) o;
-        return tmp.stack == stack;
-    } // TODO!!! java.lang.AssertionError: two empty stacks must be equal;
+        return o instanceof DoubleStack && stack.equals(((DoubleStack) o).stack);
+    }
 
     @Override
     public String toString() {
@@ -72,7 +72,7 @@ public class DoubleStack {
         bld.append("[");
         for (int i = stack.size() - 1; i >= 0; i--) {
             bld.append(stack.get(i));
-            if (stack.peek() != stack.get(i)) {
+            if (!stack.peek().equals(stack.get(i))) {
                 bld.append(", ");
             }
         }
@@ -80,10 +80,21 @@ public class DoubleStack {
         return bld.append("]").toString();
     }
 
-    public static double interpret (String pol) {
-        return 0.; // TODO!!! Your code here!
+    public static double interpret(String pol) {
+        if (!pol.matches("[\\d\\s*.+/-]+")) throw new RuntimeException();
+        DoubleStack stack = new DoubleStack();
+        String[] tokens = pol.split(" +");
+        for (String s : tokens) {
+            if (s.matches("[*+/-]$")) {
+                stack.op(s);
+            } else if (s.length() > 0) {
+                stack.push(Double.parseDouble(s));
+            }
+        }
+
+        double result = stack.pop();
+        if (!stack.stEmpty()) throw new RuntimeException();
+        return result;
     }
 
 }
-
-
